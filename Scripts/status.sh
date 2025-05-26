d1b18b4ce112a6cd412b81aba5afe628e1c5da7a
rft0 <rafos111@hotmail.com>
2025-05-26 13:54:12 +0300

    Delete Scripts/status.sh

--- Scripts/status.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-i3status | while :
-do
-    read line
-    vol=$(pactl get-sink-volume @DEFAULT_SINK@ | awk -F' ' '{print $5}' | awk -F'%' '{print $1}')
-    echo "Vol: $vol | $line" || exit 1
-done
